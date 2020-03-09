3b3e9b0224571c7bcba739e7e2d45773a4935e64
Victoria <vicglee1234007@gmail.com>
2020-03-09 12:08:29 -0500

    connected bash.js to pwd.js

--- bash.js
+++ bash.js
@@ -1,8 +1,13 @@
-process.stdout.write('prompt > ');
+const input = require("./pwd");
+input();
+// process.stdout.write('prompt > ');
 
-process.stdin.on('data', (data) => {
-    const cmd = data.toString().trim();
+// process.stdin.on('data', (data) => {
+//     const cmd = data.toString().trim();
+//     if(cmd==='pwd'){
+//         input();
+//     }
 
-    process.stdout.write(`${process.cwd()}`)
-    process.stdout.write('\nprompt > ');
-})
+//     process.stdout.write(`${process.cwd()}`)
+//     process.stdout.write('\nprompt > ');
+// })
